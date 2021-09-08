f4fd3b2b09e37345d52c46bbdc9055e91dbe09f9
RASCALCS <63025037+kashmir1cs@users.noreply.github.com>
2021-09-08 18:40:35 +0900

    xml 파싱 예제 작성

--- .PEACHES/UseXML.cs
+++ .PEACHES/UseXML.cs
@@ -8,19 +8,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml;
+using System.Xml; // xml 읽어오는 기본 어셈블리
 using System.IO;
 XmlDocument Xml = new XmlDocument(); // xml 객체 선언
 Xml.Load(xmlpath); //xml 파일 불러오기
 XmlNodeList xmlList = Xml.SelectNodes("/Path1/Path2");
+
 Console.WriteLine(Xml.SelectSingleNode("/Path1/Path2").InnerText); //console에 text 표시
 
 // 반복문 활용하여 xml tag 읽어오기
 foreach (XmlNode xmlElem in xmlList)
-                {
-                    tbCheckStatus.Text += "WELD SKEY : " + xmlElem["Tag1"].InnerText + "/ Type : " + xmlElem["Tag1"]["Tag2"].InnerText+Environment.NewLine;
-                
-                    List.xmlTagList.Add(xmlElem["Tag1"].InnerText); //List에 추가
-                    
-
-                }
+  {
+    // winform의 Text Box등에 표시
+    tbCheckStatus.Text += "파싱결과" + xmlElem["Tag1"].InnerText + " / " + xmlElem["Tag1"]["Tag2"].InnerText+Environment.NewLine;
+    List.xmlTagList.Add(xmlElem["Tag1"].InnerText); //List에 추가
+  }
